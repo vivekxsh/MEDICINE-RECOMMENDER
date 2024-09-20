20215f6ecc6e64828276aab2899c8c5f6126c2ac
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2024-09-21 01:54:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 
 # import streamlit as st
 
-from flask import Flask, request, render_template, jsonify  # Import jsonify
+# from flask import Flask, request, render_template, jsonify  # Import jsonify
+from flask import Flask, render_template  # Import jsonify
 import numpy as np
 import pandas as pd
 import pickle
